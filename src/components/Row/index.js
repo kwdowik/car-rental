0d324f4f6f68f158b3a4de53d05558a9e20741c4
Kacper Wdowik <wdowikkacper@gmail.com>
2020-11-12 18:51:40 +0100

    feat: add Row component

---
+++ src/components/Row/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Row.module.css'
+import classnames from 'classnames'
+
+export const Row = ({ type, item }) => {
+  return  (
+    <span className={classnames(styles.primary, styles[type])}>
+      [{item.id}] - {item.name}
+    </span>
+  )
+}
